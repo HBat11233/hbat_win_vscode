4c54b6709d980b04f292e357d2830230ee381c42
HBat11233 <33086330+HBat11233@users.noreply.github.com>
2019-04-17 01:23:42 +0800

    mfctest2

--- vs_pro/MFCtest2/MFCtest2/MFCtest2View.cpp
+++ vs_pro/MFCtest2/MFCtest2/MFCtest2View.cpp
@@ -26,14 +26,18 @@ BEGIN_MESSAGE_MAP(CMFCtest2View, CView)
 	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
 	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
 	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONUP()
+	ON_WM_RBUTTONDOWN()
 END_MESSAGE_MAP()
 
 // CMFCtest2View 构造/析构
 
 CMFCtest2View::CMFCtest2View() noexcept
+	:buttonBool(FALSE), MemDC(NULL), MemBitmap(NULL)
 {
 	// TODO: 在此处添加构造代码
-
 }
 
 CMFCtest2View::~CMFCtest2View()
@@ -56,17 +60,46 @@ void CMFCtest2View::OnDraw(CDC* pDC)
 	ASSERT_VALID(pDoc);
 	if (!pDoc)
 		return;
-
-	CRect rect;
 	GetClientRect(&rect);
-	CDC MemDC;
-	CBitmap MemBitmap;
+	//首先定义一个显示设备对象
+	MemDC = new CDC;
+	//定义一个位图对象
+	MemBitmap = new CBitmap;
+	//建立一个与屏幕显示兼容的内存显示设备，这是还不能绘图，因为没有地方画
+	MemDC->CreateCompatibleDC(NULL);
+	//建立一个与屏幕显示器兼容的位图，至于位图的大小，可以用窗口的大小
+	MemBitmap->CreateCompatibleBitmap(pDC, rect.Width(), rect.Height());
+	//将位图选入到内存设备中
+	//只有选入了位图的内存显示设备才有地方绘图，画到指定位图上
+	CBitmap *pOldBit = MemDC->SelectObject(MemBitmap);
+	//先用背景色将位图清除干净，这里用白色作为背景
+	MemDC->FillSolidRect(rect, RGB(255, 255, 255));
 
-	MemDC.CreateCompatibleDC(NULL);
+	//绘图
+	
+	int length = buttonS.size();
+	
+	if (length > 0)
+	{
+		MemDC->MoveTo(buttonS[0]);
+		for (int i = 1; i < length; ++i)
+			MemDC->LineTo(buttonS[i]);
+		if (buttonBool)
 
-	MemBitmap.CreateCompatibleBitmap(pDC, rect.Width(), rect.Height());
+			MemDC->LineTo(buttonM);
+	}
 
-	CBitmap *pOldBit = MemDC.SelectObject(&MemBitmap);
+
+
+
+	//将内存中的图拷贝到屏幕上进行显示
+	pDC->BitBlt(0, 0, rect.Width(), rect.Height(), MemDC, 0, 0, SRCCOPY);
+	MemBitmap->DeleteObject();
+	delete MemBitmap;
+	MemBitmap = NULL;
+	MemDC->DeleteDC();
+	delete MemDC;
+	MemDC = NULL;
 
 	// TODO: 在此处为本机数据添加绘制代码
 }
@@ -113,3 +146,71 @@ CMFCtest2Doc* CMFCtest2View::GetDocument() const // 非调试版本是内联的
 
 
 // CMFCtest2View 消息处理程序
+
+
+void CMFCtest2View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	buttonBool = TRUE;
+	buttonS.push_back(buttonM);
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void CMFCtest2View::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	if (buttonS.size() > 0)
+	{
+		if (nFlags == MK_SHIFT)
+		{
+			if (abs(buttonS.rbegin()->x - point.x) < abs(buttonS.rbegin()->y - point.y))
+				buttonM = CPoint(buttonS.rbegin()->x, point.y);
+			else
+				buttonM = CPoint(point.x, buttonS.rbegin()->y);
+		}
+		else
+		{
+			buttonM = buttonS[0];
+			for (CPoint i : buttonS)
+			{
+				int dx = abs(i.x - point.x), dy = abs(i.y - point.y);
+				if (dx <= 5 && dy <= 5)
+					if (sqrt(dx*dx + dy * dy) <= sqrt(pow((buttonM.x - point.x), 2) + pow((buttonM.y - point.y), 2)))
+						buttonM = i;
+			}
+			if (abs(buttonM.x - point.x) > 5 || abs(buttonM.y - point.y) > 5)
+				buttonM = point;
+		}
+
+	}
+	else
+		buttonM = point;
+	if (buttonBool)Invalidate(TRUE);
+	CView::OnMouseMove(nFlags, point);
+}
+
+
+void CMFCtest2View::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CView::OnLButtonUp(nFlags, point);
+}
+
+
+void CMFCtest2View::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	buttonBool = FALSE;
+	Invalidate(TRUE);
+	CView::OnRButtonDown(nFlags, point);
+}
+
+
+VOID CMFCtest2View::myFill()
+{
+	// TODO: 在此处添加实现代码
+	std::vector<CPoint> que(buttonS);
+
+	return;
+}
--- vs_pro/MFCtest2/MFCtest2/MFCtest2View.h
+++ vs_pro/MFCtest2/MFCtest2/MFCtest2View.h
@@ -3,7 +3,8 @@
 //
 
 #pragma once
-
+#include <vector>
+#include <algorithm>
 
 class CMFCtest2View : public CView
 {
@@ -40,6 +41,23 @@ protected:
 // 生成的消息映射函数
 protected:
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+private:
+	BOOL buttonBool;
+	CPoint buttonM;
+	CRect rect;
+	// //建立一个显示设备的对象
+	CDC* MemDC;
+	// //定义一个位图对象
+	CBitmap* MemBitmap;
+	// //point集合
+	std::vector<CPoint> buttonS;
+public:
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	VOID myFill();
 };
 
 #ifndef _DEBUG  // MFCtest2View.cpp 中的调试版本
