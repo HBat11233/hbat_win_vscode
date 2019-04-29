708990d71c0468160919b6e699e9c22755c4ef4b
HBat11233 <33086330+HBat11233@users.noreply.github.com>
2019-04-29 22:25:01 +0800

    vivado

--- vivado/fourToOne/fourToOne.srcs/sources_1/new/fourToOne.v
+++ vivado/fourToOne/fourToOne.srcs/sources_1/new/fourToOne.v
@@ -21,13 +21,16 @@
 
 
 module fourToOne(
-    input a,
-    input b,
-    input c,
-    input d,
+    input a[2:0],
+    input b[2:0],
+    input c[2:0],
+    input d[2:0],
     input x,
     input y,
-    output o
+    output o[2:0]
     );
-    assign o=(~x&~y&a)|(x&~y&b)|(~x&y&c)|(x&y&d);
+    assign o[0]=(~x&~y&a[0])|(x&~y&b[0])|(~x&y&c[0])|(x&y&d[0]);
+    assign o[1]=(~x&~y&a[1])|(x&~y&b[1])|(~x&y&c[1])|(x&y&d[1]);
+    assign o[2]=(~x&~y&a[2])|(x&~y&b[2])|(~x&y&c[2])|(x&y&d[2]);
+    
 endmodule
